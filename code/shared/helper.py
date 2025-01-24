de796670fd9a7ad49da251e29e9672eb47145f1d
Jonathan Feilmeier <jonathan@feilmeier.eu>
2025-01-24 13:19:36 +0100

    fixed helper due to zero division and empty data

--- code/shared/helper.py
+++ code/shared/helper.py
@@ -8,14 +8,20 @@ def read_data(file_name, directory):
 
 
 def check_imbalance(y_data):
+    assert len(y_data) > 0, "data should not be empty for balance check"
+
     counts = np.bincount(y_data)
-    num_zeros, num_ones = counts[0], counts[1]
+    num_zeros = counts[0]
+    if counts.size == 2:
+        num_ones = counts[1]
+    else:
+        num_ones = 0
 
     if num_zeros < num_ones:
         minority_label = 0
-        threshold = num_zeros / num_ones
+        threshold = num_zeros / (num_ones + 1e-13)
     else:
         minority_label = 1
-        threshold = num_ones / num_zeros
+        threshold = num_ones / (num_zeros + 1e-13)
 
     return minority_label, threshold
