ef75f6ef1495871cdb457b452705d4cde68ecca8
Jonathan Feilmeier <jonathan@feilmeier.eu>
2025-02-28 20:24:09 +0100

    params

--- code/scripts/slurm_job_batch.sh
+++ code/scripts/slurm_job_batch.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH --job-name=fly-smote-parallel
-#SBATCH --output=/bigwork/nhwast10/logs/job_output_%A/job_output_%A_%a.log   # Log-Dateien für jeden Task
-#SBATCH --error=/bigwork/nhwast10/logs/job_error_%A/job_error_%A_%a.log     # Fehler-Log für jeden Task
-#SBATCH --array=0-4                     # Array mit 10 Jobs (ID von 0 bis 9)
-#SBATCH --cpus-per-task=32              # 4 CPUs pro Task
-#SBATCH --mem-per-cpu=4G                # Speicher pro Task
-#SBATCH --time=4:00:00                 # Maximale Laufzeit
+#SBATCH --output=/bigwork/nhwast10/logs/job_output_%A/job_output_%A_%a.log
+#SBATCH --error=/bigwork/nhwast10/logs/job_error_%A/job_error_%A_%a.log
+#SBATCH --array=0-4
+#SBATCH --cpus-per-task=32
+#SBATCH --mem-per-cpu=4G
+#SBATCH --time=4:00:00
 #SBATCH --mail-user=jonathan.feilmeier@stud.uni-hannover.de
 #SBATCH --mail-type=ALL
 
@@ -15,4 +15,4 @@ conda activate myenv
 cd $BIGWORK/FLY-SMOTE-CCMCB_compass_multiple
 
 # Python-Skript für den aktuellen Task ausführen
-python -m code.scripts.runner_parallel --param_file "./config/compass/params_multiple.json" --max_workers 3 --num_tasks 5 --task_id $SLURM_ARRAY_TASK_ID
+python -m code.scripts.runner_parallel --param_file "./config/compass/params_multiple.json" --max_workers 3 --total_tasks 5 --task_id $SLURM_ARRAY_TASK_ID
